0551f6b49f75c4e1a706977729d56a35b8fb5004
Elvis Paico <epaicoca@emeal.nttdata.com>
2023-06-26 16:01:25 -0500

    Add ProfileCustomer

---
+++ src/main/java/com/bank/models/bean/ProfileCustomer.java
@@ -0,0 +1,13 @@
+package com.bank.models.bean;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProfileCustomer {
+    private String codPerfil;
+    private String desPerfil;
+}
